5dd5307ec26617077581b7d5fd8781c06ecd733b
ahmedseragcodes <ahmedserag1@gmail.com>
2021-03-16 15:49:47 -0400

    installed dependencies

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const ADD_FEATURE="ADD_FEATURE";
+export const REMOVE_FEATURE="REMOVE_FEATURE";
+
+
+export const addFeature= (featureToAdd)=>{
+    return ({ type: ADD_FEATURE, payload: featureToAdd })
+}
+
+export const removeFeature= (featureToRemove)=>{
+    return ({ type: REMOVE_FEATURE, payload: featureToRemove })
+}
+
